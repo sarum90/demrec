0c067ea9806acc3f49efc773ed8ebd66ab789d38
Marcus Ewert <marcus@scanner.dev>
2025-07-22 19:52:40 -0700

    really nice

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use wasm_bindgen::JsCast;
 use wasm_bindgen::closure::Closure;
 
 // const FAVICON: Asset = asset!("/assets/favicon.ico");
-// const MAIN_CSS: Asset = asset!("/assets/main.css");
+const MAIN_CSS: Asset = asset!("/assets/main.css");
 
 fn main() {
     dioxus::launch(App);
@@ -13,7 +13,7 @@ fn main() {
 fn App() -> Element {
     rsx! {
         // document::Link { rel: "icon", href: FAVICON }
-        // document::Link { rel: "stylesheet", href: MAIN_CSS }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
         DraggableCircle {}
     }
 }
